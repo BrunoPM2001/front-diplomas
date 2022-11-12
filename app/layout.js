a55c8a1d7a1545e422097465b53cab7b544a3f88
Bruno P.M <brunoporras2001@gmail.com>
2022-11-11 23:53:34 -0500

    Layout de la página inicial

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import styles from '../styles/globals.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head></head>
+      <body>{children}</body>
+    </html>
+  )
+}
