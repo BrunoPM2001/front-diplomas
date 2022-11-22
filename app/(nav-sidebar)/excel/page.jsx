dd3036a3a6b55db5939923fc5e0b49977e03bef3
Bruno P.M <brunoporras2001@gmail.com>
2022-11-22 08:10:07 -0500

    Vista de la opción de excel

--- app/(nav-sidebar)/excel/page.jsx
+++ app/(nav-sidebar)/excel/page.jsx
@@ -1,5 +1,38 @@
+import styles from '../../../styles/Excel.module.css'
+import LoadCard from "../../../components/excel/cards";
+
+const cards = [
+  {
+    tittle: "Cargar padrones",
+    accion: "Cargar",
+    desc: "Registrar nuevos padrones(se obviarán los duplicados)."
+  },
+  {
+    tittle: "Cargar padrones",
+    accion: "Cargar",
+    desc: "Registrar nuevos padrones(se obviarán los duplicados)."
+  },
+  {
+    tittle: "Cargar padrones",
+    accion: "Cargar",
+    desc: "Registrar nuevos padrones(se obviarán los duplicados)."
+  }
+]
+
 export default function Page () {
   return (
-    <div>Hi, i'm in excel window</div>
+    <div className={styles.container}>
+      {
+        cards.map(element => {
+          return (
+            <LoadCard
+              tittle={element.tittle}
+              accion={element.accion}
+              desc={element.desc}
+            />
+          )
+        })
+      }
+    </div>
   )
 }
