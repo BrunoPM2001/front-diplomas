726780c406c9a045e95d879b8ae579f48279f1ff
Bruno P.M <brunoporras2001@gmail.com>
2022-11-22 08:12:31 -0500

    Layout global

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,19 @@
+import { Roboto }  from '@next/font/google'
 import styles from '../styles/globals.css'
+
+const font = Roboto({
+  weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin']
+})
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head></head>
-      <body>{children}</body>
+      <body className={font.className}>
+        {children}
+      </body>
     </html>
   )
 }
