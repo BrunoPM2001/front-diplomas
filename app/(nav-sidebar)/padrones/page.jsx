ef0bc4f31b59ebe1353f478572680821a3f562da
Bruno P.M <brunoporras2001@gmail.com>
2022-11-22 16:58:33 -0500

    Carga de la lista de padrones - ok

--- app/(nav-sidebar)/padrones/page.jsx
+++ app/(nav-sidebar)/padrones/page.jsx
@@ -1,11 +1,27 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import { getAllP } from "../../../api/padrones";
 import Resultados from "../../../components/padrones/resultados";
 import SearchPadron from "../../../components/padrones/search";
 
 export default function Page () {
+
+  const [padrones, setPadrones] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getAllP().then(data => setPadrones(data))
+    setLoading(false)
+  }, [])
+
   return (
     <div className="bodyContent">
       <SearchPadron/>
-      <Resultados/>
+      <Resultados
+        loading={loading}
+        padrones={padrones}
+      />
     </div>
   )
 }
