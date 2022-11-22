0ce8b97ac9f224f4fad3a33cb655243b89857b2c
Bruno P.M <brunoporras2001@gmail.com>
2022-11-22 13:42:27 -0500

    Interacción de las cards - ok

--- components/excel/cards.jsx
+++ components/excel/cards.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import Dropzone from "./dropzone"
 import styles from "../../styles/Excel.module.css"
+
 export default function LoadCard ( props ) {
+
+  //  Archivo
+  const [file, setFile] = useState(null)
+  
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardHeader}>
         <h2>{props.tittle}</h2>
-        <p>{props.accion}</p>
+        <p onClick={() => props.method(file)}>{props.accion}</p>
       </div>
       <p className={styles.cardDesc}>{props.desc}</p>
-      <Dropzone/>
+      <Dropzone file={file} setFile={setFile}/>
     </div>
   )
 }
