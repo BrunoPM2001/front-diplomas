3e2e28983517912f18a5c29182ebe8a1386f0108
Bruno P.M <brunoporras2001@gmail.com>
2022-11-23 00:57:38 -0500

    Layout dentro de la app

--- app/(nav-sidebar)/layout.js
+++ app/(nav-sidebar)/layout.js
@@ -1,13 +1,21 @@
+import { Roboto }  from '@next/font/google'
 import Sidebar from "../../components/general/Sidebar"
 import Navbar from "../../components/general/Navbar"
 import StatusBar from "../../components/general/Statusbar"
 
+const font = Roboto({
+  weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['latin']
+})
+
 export default function RootLayout({ children }) {
+
 
   return (
     <html>
       <head></head>
-      <body>
+      <body className={font.className}>
         <div className="sideBody">
           <Sidebar/>
           <div className="bodyParte">
