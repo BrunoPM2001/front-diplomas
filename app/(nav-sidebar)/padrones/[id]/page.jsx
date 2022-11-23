5ff06473342bedd7b9094f24bd94692772c7c711
Bruno P.M <brunoporras2001@gmail.com>
2022-11-23 00:58:12 -0500

    Página de un padrón

--- app/(nav-sidebar)/padrones/[id]/page.jsx
+++ app/(nav-sidebar)/padrones/[id]/page.jsx
@@ -1,13 +1,36 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import { getOneP } from '../../../../api/padrones'
+import { usePadron } from '../../../../hooks/useForm'
 import BodyPadron from '../../../../components/padrones/bodyPadron'
 import HeadPadron from '../../../../components/padrones/headPadron'
 import styles from '../../../../styles/Padrones.module.css'
 
-export default function Page () {
+export default function Page ({ params }) {
+  
+  const { padron, setPadron, updatePadron } = usePadron()
+  const [loading, setLoading] = useState(true)
+
+  
+  useEffect(() => {
+    getOneP(params.id).then(data => setPadron(data))
+    setLoading(false)
+  }, [])
+
   return (
     <div className="bodyContent">
       <div className={styles.padronContainer}>
-        <HeadPadron/>
-        <BodyPadron/>
+        <HeadPadron
+          loading={loading}
+          padron={padron}
+          reg={params.id}
+        />
+        <BodyPadron
+          loading={loading}
+          padron={padron}
+          update={updatePadron}
+        />
       </div>
     </div>
   )
