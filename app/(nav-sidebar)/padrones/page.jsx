51aa85cd768f108018407d38d977e6ac0f077cc2
Bruno P.M <brunoporras2001@gmail.com>
2022-11-23 00:58:31 -0500

    Página de búsqueda de padrones

--- app/(nav-sidebar)/padrones/page.jsx
+++ app/(nav-sidebar)/padrones/page.jsx
@@ -1,15 +1,17 @@
 "use client"
 
-import { useState, useEffect } from "react";
-import { getAllP } from "../../../api/padrones";
-import Resultados from "../../../components/padrones/resultados";
-import SearchPadron from "../../../components/padrones/search";
+import { useState, useEffect } from "react"
+import { getAllP } from "../../../api/padrones"
+import Resultados from "../../../components/padrones/resultados"
+import SearchPadron from "../../../components/padrones/search"
 
 export default function Page () {
 
+  //  Estados para la data del padrón y su carga
   const [padrones, setPadrones] = useState()
   const [loading, setLoading] = useState(true)
 
+  //  Llamada a la API
   useEffect(() => {
     getAllP().then(data => setPadrones(data))
     setLoading(false)
